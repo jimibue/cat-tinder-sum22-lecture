f9d256690c27f03db9ccaf1874cc2548fbf8de37
James <jimibue@gmail.com>
2022-06-21 11:35:15 -0600

    cats frontend

--- client/src/components/shared/UserAccount.js
+++ client/src/components/shared/UserAccount.js
@@ -1,12 +1,30 @@
-import { useContext } from "react";
+import axios from "axios";
+import { useContext, useEffect, useState } from "react";
 import { AuthContext } from "../../providers/AuthProvider";
 
 const UserAccount = () => {
   const {user} = useContext(AuthContext)
+  const [cats, setCats] = useState([])
+
+  useEffect(()=>{
+    getCats()
+  },[])
+  const getCats = async()=>{
+    try{
+      // remeber we are sending the token with this
+      // in our req.headers
+      let res = await axios.get('/api/cats')
+      setCats(res.data)
+    }catch(err){
+      alert('err occured')
+      console.log(err)
+    }
+  }
   return (
       <div>
       <h1>User Account Settings: you must be logged to see this</h1>
       <p>Hello {user.email}</p> 
+      <p>{JSON.stringify(cats)}</p>
       </div>
   )
 }
